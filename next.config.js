4bc5fdf5ed12acf8e9c94d1a27b3a8ea16324ed0
Ashikur Rahman Ovi <ashikurovi2003@gmail.com>
2026-02-27 23:53:31 +0600

    make user api

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const nextConfig = {
       // Allow CDN used by backend for product images (both http and https just in case)
       { protocol: "https", hostname: "squadlog-cdn.up.railway.app", pathname: "/uploads/**" },
       { protocol: "http", hostname: "squadlog-cdn.up.railway.app", pathname: "/uploads/**" },
+      { protocol: "https", hostname: "e-cdn.vercel.app", pathname: "/uploads/**" }
       // Backend uploads / external CDN (Top Products, Media, etc.)
       { protocol: "https", hostname: "squadcart-backend.up.railway.app", pathname: "/uploads/**" },
       { protocol: "http", hostname: "squadcart-backend.up.railway.app", pathname: "/uploads/**" },
